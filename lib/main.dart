5df636cb0e1eb9b3769547e3c39d47038325334f
Kurt Bermudez <kurt@wizy.io>
2023-12-23 16:15:39 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const text = Text('Design Patterns');
+    return MaterialApp(
+      title: 'SOLID and Design Patterns',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: text,
+        ),
+      ),
+    );
+  }
+}
